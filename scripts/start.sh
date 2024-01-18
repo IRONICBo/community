c917b0dc0c8a1581405b43ff12d5227af59f1fee
linjie <lj7674@126.com>
2024-01-18 23:36:43 +0800

    start frontend program in the background and rediret output to frontend.log

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,2 +1,2 @@
-cd cmd/gopcomm && gop run . &
-cd cmd/gopcomm/yap/community-frontend && npm install && npm run dev
+cd cmd/gopcomm/yap/community-frontend && npm install && nohup npm run dev >frontend.log 2>&1 &
+cd cmd/gopcomm && gop run .
